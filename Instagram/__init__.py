108a50f3854c9a831726b5c5650109e87117f12f
Shah <kalpanshah@Kalpans-MacBook-Air.local>
2020-08-15 20:18:59 +0530

    feat: :sparkles: Added Test for For Our Instagram Post Template

--- Instagram/__init__.py
+++ Instagram/__init__.py
@@ -1,4 +1,4 @@
-from Instagram.Template01.Temaplete01 import Template as SimpleDesign
+from Instagram.Templates.Temaplete01 import Template as SimpleDesign
 
 if __name__ == '__main__':
     post = SimpleDesign().generate_post()
