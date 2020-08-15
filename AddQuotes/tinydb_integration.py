697df1603f0f967caa1000f6650895a754ac7da7
Riken Shah <rikenshah.02@gmail.com>
2020-08-16 02:55:14 +0530

    fix: :bug:Made a template to add quotes from api and integrated tinydb

--- AddQuotes/tinydb_integration.py
+++ AddQuotes/tinydb_integration.py
@@ -1,40 +1,31 @@
 from AddQuotes.add_quotes_from_api import AddFromApi
-import requests
-import json
 from datetime import datetime
+from tinydb import TinyDB
+from Database.tinyDb import QuotesDatabase
 
 
-class TinyDB:
-    def __init__(self, url, methods, headers, filename):
-        self.response = AddFromApi.send_request(requests, url=url, method=methods, headers=headers)
-        self.filename = filename
-        if self.response.status_code != 200:
-            ConnectionError('API did not give the correct response')
-        self.__write_json_file()
+class QuotesTinyDb(AddFromApi):
+    def __init__(self, *args, filename='', **kwargs):
+        self.db = QuotesDatabase(filename)
+        super().__init__(*args, **kwargs)
 
-    def __write_json_file(self):
-        data = self.response.content
-        if not data:
-            return ValueError('Response has no data.')
+    def _write_to_db(self, quotes, quote_key, author_key):
+        self.db.add_list(quotes, quote_key, author_key)
+        return True
 
-        clean_data = self.__clean_data(json.loads(data))
-        with open(self.filename, 'w+') as file:
-            for line in json.dumps(clean_data, sort_keys=True, indent=4, default=str):
-                file.write(line)
-
-    @staticmethod
-    def __clean_data(data):
-        new_data = []
-        index = 0
-        for quote in data:
-            quote['id'] = index
-            quote['used_on_insta'] = False
-            quote['timestamp'] = datetime.now()
-            new_data.append(quote)
-            index += 1
-        return new_data
+    # @staticmethod
+    # def __clean_data(data):
+    #     new_data = []
+    #     index = 0
+    #     for quote in data:
+    #         quote['id'] = index
+    #         quote['used_on_insta'] = False
+    #         quote['timestamp'] = str(datetime.now())
+    #         new_data.append(quote)
+    #         index += 1
+    #     return new_data
 
 
 if __name__ == '__main__':
     url = 'https://type.fit/api/quotes'
-    test = ApiToJson(url, 'get', None, filename="quotes_data.json")
+    test = QuotesTinyDb(url, method='get', filename="../Database/quotes_data.json", quote_key='text')
