96818d2fda129aa156ef3110eb25c9e27986c8ef
Riken Shah <rikenshah.02@gmail.com>
2020-08-19 17:19:53 +0530

    :racehorse: Enhace the test

--- Instagram/__init__.py
+++ Instagram/__init__.py
@@ -2,8 +2,8 @@ from Instagram.Templates.Temaplete01 import Template as SimpleDesign
 
 if __name__ == '__main__':
     template = SimpleDesign(for_test=False)
-    for i in range(1, 100):
-        post = template.generate_post()
-        post.save(f'Test/test{i}.png')
+    for i in range(1, 101):
+        did_create = template.generate_post()
+        if did_create:
+            did_create.save(f'Test/test{i}.png')
         template.update_everything()
-        print(f'{(101 - i - 1)/100 * 100}% left', i)
