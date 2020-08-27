c09ef8a3d91ad581ad09b5c7fa8627cb5c517b8a
Riken Shah <rikenshah.02@gmail.com>
2020-08-27 18:58:00 +0530

    :sparkles: feat: Added Logger and Scheduler

--- create_post.py
+++ create_post.py
@@ -1,7 +1,7 @@
-import os
 from Scripts.Instagram.Templates.Temaplete01 import Template as SimpleDesign
 from Scripts.Instagram.CaptionCreator import BasicCaption
 from Scripts.Instagram.Bot import Bot
+import logging
 
 
 class CreatePost:
@@ -14,19 +14,25 @@ class CreatePost:
 
         # Creating a bot instance
         self.post_bot = Bot(testing=testing)
+        logging.info('Created a Selenium Instance')
 
     def create(self, img_path, counter=0):
         did_create = self.template.generate_post()
         if did_create:
             did_create.save(img_path)
-            print(self.template.quote, self.template.author)
             caption = self.caption_template.create_caption(self.template.author)
-            self.post_bot.post(img_path, caption)
+            try:
+                self.post_bot.post(img_path, caption)
+            except Exception:
+                logging.exception('Something Went Wrong with the Bot')
+                logging.info('Restarting the bot...')
+                self.post_bot = Bot(testing=False)
+                self.create(img_path, counter=counter)
             self.template.update_everything()
             return True
         # Re-run for 10 times if it failed
         elif counter < 10:
-            print(f'Failed for creating a post... Running again for {counter} time.')
+            logging.error(f'Failed for creating a post... Running again for {counter} time.')
             counter += 1
             return self.create(img_path, counter=counter)
         return False
--- run.py
+++ run.py
@@ -1,16 +1,24 @@
 import os
+import logging
 from create_post import CreatePost
+import schedule
 
 if __name__ == '__main__':
-    post = CreatePost()
-    for i in range(1):
-        path = os.path.abspath(os.getcwd()) + '/' + f'test{i + 1}.png'
-        post.create(path)
-    post.post_bot.exit()
+    # Setting Up a LogFile
+    logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w',
+                        format='%(asctime)s - %(name)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logging.info('CREATED LOGFILE')
 
-""" 
-TODO: 
-1) Create a better file structure 
-3) Finding a way to make every task occur at specific time 
-4) Adding requirements.txt 
-"""
+    post = None
+    counter = 0
+    try:
+        post = CreatePost()
+    except Exception as e:
+        logging.exception('Error in CreatePost instance')
+    if post:
+        path = os.path.abspath(os.getcwd()) + '/post.png'
+        schedule.every(6).hours.do(post.create, path)
+        post.create(path)
+
+        while True:
+            schedule.run_pending()
