69a73ec47a8fe2450276d907ae4abffdec7c9d90
Riken Shah <rikenshah.02@gmail.com>
2020-08-29 14:18:26 +0530

    :arrow_up: update: Added the condition to avoid to go to the bot is no new user is found

--- Scripts/Helpers/greetings.py
+++ Scripts/Helpers/greetings.py
@@ -5,4 +5,5 @@ from Scripts.Instagram.InstagramAPI import InstagramAPIBot
 def greeting_to_new_users(bot: Bot, api_bot: InstagramAPIBot):
     new_users = api_bot.get_new_user_followers()
     new_users = list(map(lambda user: user['username'], new_users))
-    bot.first_time_following(new_users)
+    if new_users:
+        bot.first_time_following(new_users)
