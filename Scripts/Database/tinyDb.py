4ff98809b5ec7e6626ab4cb915f0872fd7c5b5d0
Riken Shah <rikenshah.02@gmail.com>
2020-12-31 00:26:43 +0530

    🔨 Improved the database scripts

--- Scripts/Database/tinyDb.py
+++ Scripts/Database/tinyDb.py
@@ -16,45 +16,29 @@ class QuotesDatabase(Database):
             logging.info('Creating new database file.')
         self.db = TinyDB(db_path)
         self.query = Query()
-        if 'for_test' in kwargs:
-            self.test_mode = kwargs['for_test']
-        else:
-            self.test_mode = False
 
-    # Check if quote exist
     def _check_quote_exist(self, quote):
-        """
-        Check is some quote exists
-        """
-        quotes = self.db.search(self.query.quote == quote)
-        for _ in quotes:
-            return True
-        return False
+        return bool(self.db.search(self.query.quote == quote))
 
-    # Add [quote, author] to our db
     def _add_quote(self, quote: str, author: str):
-        """
-        Add single quote to the db
-        """
-
         try:
             self.db.insert({'quote': quote, 'author': author, 'used_on_insta': False, 'timestamp': str(datetime.now())})
         except Exception as e:
-            print(f'Error occurred while adding this quote -> {quote}, \n Error -> {e}')
+            logging.error(f'Error occurred while adding quote -> {quote}, \n Error -> {e}')
             return False
         return True
 
-    # Fetch the new quote
     def fetch_quote(self):
         """
-        If no quotes are left in db it then it will return false
-        :return quote or False:
+        If no quotes are left in db it then it will return random quote
+        :return [quote, author] dict
         """
         quotes = self.db.search(self.query.used_on_insta == False)
-        for quote in quotes:
-            self.db.update({'used_on_insta': not self.test_mode}, doc_ids=[quote.doc_id])
-            return {'quote': quote['quote'], 'author': quote['author']}
-        return False
+        if not quotes:
+            quotes = self.db.search(self.query.used_on_insta == True)
+        quote = quotes[0]
+        self.db.update({'used_on_insta': True}, doc_ids=[quote.doc_id])
+        return {'quote': quote['quote'], 'author': quote['author']}
 
     def is_empty(self):
         return len(self.db.all()) == 0
